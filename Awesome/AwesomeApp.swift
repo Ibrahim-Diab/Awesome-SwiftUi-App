83503eac93603b037f29d0753907e342b5a7d6db
Ibrahim diab <ibrahimdiab011@gmail.com>
2024-01-16 16:53:12 +0200

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Diab on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
